59343e15a64f5613d557802b67b1f632aebed7c4
viktoriyaoreshkina <vikus-jesus@mail.ru>
2014-10-20 15:53:31 +0600

    Chief-cooker
    Test-task

---
+++ Korneplodi.java
@@ -0,0 +1,12 @@
+package simple;
+
+/**
+ * Created by Viktoriya_Oreshkina on 19-Oct-14.
+ */
+public class Korneplodi extends Vegetables {
+    protected String nameK;
+
+    public String getNameK() {
+        return nameK;
+    }
+}

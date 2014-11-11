ddc2833067076b2351515e5ff82cd4c92b9991fb
Viktoriya Oreshkina <vikus-jesus@mail.ru>
2014-11-11 13:29:26 +0600

    missed classes was added

---
+++ src/main/java/simple/Bahchevie.java
@@ -0,0 +1,12 @@
+package simple;
+
+/**
+ * Created by Viktoriya_Oreshkina on 19-Oct-14.
+ */
+public class Bahchevie extends Vegetables {
+    protected String nameB;
+
+    public String getNameB() {
+        return nameB;
+    }
+}
